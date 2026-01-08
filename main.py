5b2f53f190c97b18bcd32aa91658b7199579e556
camila-carrillo <carrillo.ca@northeastern.edu>
2026-01-08 14:35:48 -0500

    lesson 1

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 """
 TODO: A very useful temperature-conversion app.
 """
+
+def main() -> None:
+    print("hello world!")
+
+    name: str = input("What is your name?")
+
+    print("hello " + name)
+
+    print(f"hello { name }")
+
+if __name__ == "__main__":
+    main()
