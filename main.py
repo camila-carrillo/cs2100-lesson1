750a7852bc76b537aa0cd249c1025e9cdc8b5e6a
Camila Carrillo <carrillo.ca@northeastern.edu>
2026-01-12 14:34:34 -0500

    tested greeting and is it cold

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 """
 TODO: A very useful temperature-conversion app.
 """
+TEMP_THRESHOLD_F: float = 68
+
+def greet_human() -> None:
+    """ get a name from the keyboard
+    and say hello! """
+    name: str = input("What is your name?")
+    print(f"hello { name }")
+
+def is_it_cold_f(temp_f: float) -> bool:
+    '''
+    Determines if the supplied temperature 
+    is below a threshold
+
+    Parameters
+    ==========
+    temp_f : float
+        supplied temperature in F
+    
+    Returns
+    =========
+    bool
+        True if it is below a threshold
+    '''
+    return temp_f < TEMP_THRESHOLD_F
+
 
 def main() -> None:
-    print("hello world!")
-
-    name: str = input("What is your name?")
-
-    print("hello " + name)
-
-    print(f"hello { name }")
+    pass # ... has the same functionality as pass!
 
 if __name__ == "__main__":
     main()
+
